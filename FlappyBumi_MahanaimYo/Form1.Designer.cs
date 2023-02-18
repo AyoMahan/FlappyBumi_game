47d9affe9b04e3f2532307de8929cead2eb68713
Mahanaim Rubin Yo <mahanaimyo@gmail.com>
2023-02-17 19:11:38 -0500

    initial commit

---
+++ FlappyBumi_MahanaimYo/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FlappyBumi_MahanaimYo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ScoreBox = new System.Windows.Forms.Label();
+            this.pipeBottom = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.bumi = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bumi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ScoreBox
+            // 
+            this.ScoreBox.AutoSize = true;
+            this.ScoreBox.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreBox.Location = new System.Drawing.Point(12, 9);
+            this.ScoreBox.Name = "ScoreBox";
+            this.ScoreBox.Size = new System.Drawing.Size(102, 35);
+            this.ScoreBox.TabIndex = 3;
+            this.ScoreBox.Text = "Score::";
+            this.ScoreBox.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pipeBottom
+            // 
+            this.pipeBottom.Image = global::FlappyBumi_MahanaimYo.Properties.Resources.pipe1;
+            this.pipeBottom.Location = new System.Drawing.Point(501, 321);
+            this.pipeBottom.Name = "pipeBottom";
+            this.pipeBottom.Size = new System.Drawing.Size(86, 163);
+            this.pipeBottom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeBottom.TabIndex = 5;
+            this.pipeBottom.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = global::FlappyBumi_MahanaimYo.Properties.Resources.pipedown;
+            this.pipeTop.Location = new System.Drawing.Point(667, 9);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(78, 209);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 4;
+            this.pipeTop.TabStop = false;
+            // 
+            // bumi
+            // 
+            this.bumi.Image = global::FlappyBumi_MahanaimYo.Properties.Resources.boston;
+            this.bumi.Location = new System.Drawing.Point(91, 190);
+            this.bumi.Name = "bumi";
+            this.bumi.Size = new System.Drawing.Size(89, 87);
+            this.bumi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bumi.TabIndex = 2;
+            this.bumi.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.Brown;
+            this.ground.Image = global::FlappyBumi_MahanaimYo.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-6, 480);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(814, 56);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(804, 532);
+            this.Controls.Add(this.pipeBottom);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.ScoreBox);
+            this.Controls.Add(this.bumi);
+            this.Controls.Add(this.ground);
+            this.Name = "Form1";
+            this.Text = "GameConsole";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pipeBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bumi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox bumi;
+        private System.Windows.Forms.Label ScoreBox;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeBottom;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ FlappyBumi_MahanaimYo/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBumi_MahanaimYo
+{
+    public partial class Form1 : Form
+    {
+        int pipeSpeed=8;
+        int gravity=15;
+        int score=0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            bumi.Top += gravity; //pushes icon down as soon as the timer starts
+            pipeBottom.Left -= pipeSpeed;//calling pipes to the other end
+            pipeTop.Left -= pipeSpeed;
+
+
+
+            if (pipeBottom.Left < -50) {
+                pipeBottom.Left = 600;
+                    }
+            if (pipeTop.Left < -80)
+            {
+                pipeTop.Left = 400;
+            }
+        }
+
+        private void gamekeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 15;
+            }
+        }
+
+        private void gameKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -15;
+            }
+        }
+    }
+}
